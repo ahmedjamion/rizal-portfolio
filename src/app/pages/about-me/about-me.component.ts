c57d9659bfcfc2e4f27e12751da10c00dcd09cd0
Ahmed Rashad Jamion <jamionahmedrashadi@gmail.com>
2025-04-11 22:10:27 +0800

    does it really matter

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { AboutCardComponent } from '../../components/about-card/about-card.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [AboutCardComponent, RouterLink],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.css'
+  styleUrl: './about-me.component.css',
 })
-export class AboutMeComponent {
-
-}
+export class AboutMeComponent {}
--- src/app/pages/about-rizal/about-rizal.component.ts
+++ src/app/pages/about-rizal/about-rizal.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { AboutRizalSectionComponent } from '../../components/about-rizal-section/about-rizal-section.component';
 import { RouterLink } from '@angular/router';
+import { AboutCardComponent } from '../../components/about-card/about-card.component';
 
 @Component({
   selector: 'app-about-rizal',
-  imports: [AboutRizalSectionComponent, RouterLink],
+  imports: [RouterLink, AboutCardComponent],
   templateUrl: './about-rizal.component.html',
   styleUrl: './about-rizal.component.css',
 })
